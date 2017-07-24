628563d71b0da4d8dbad24998f703e3797ba7cbc
StephaniePC <teff.padilla15@gmail.com>
2017-07-23 23:17:23 -0600

    Actualizacion de hojas de estilo

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20170723183449) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "mensajes", force: :cascade do |t|
     t.string "De"
     t.string "Para"
